b4f05dc3f3851ba041d9cfaf4c6b2b87929bb148
Ian Scrivener <ian.scrivener@cabcharge.com.au>
2016-06-03 10:08:39 +1000

    fix docker tag

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --rm=false -t ianscrivener/helloWorld-alpine-node6.2 .
+docker build --rm=false -t ianscrivener/helloworld-alpine-node:6.2 .
 docker images | grep ianscrivener
