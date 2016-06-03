d5772263bd12c222b0b00aef6d825aebdf830276
Ian Scrivener <ian.scrivener@cabcharge.com.au>
2016-06-03 13:21:33 +1000

    init

--- interactive-run.sh
+++ interactive-run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-docker run -it --rm=true ianscrivener/helloworld-ubuntu-node:16.04_6.x \
+docker run -it --rm=true \
+    -p 3000:3000 \
+    ianscrivener/helloworld-ubuntu-node:16.04_6.x \
     bash
 
 
